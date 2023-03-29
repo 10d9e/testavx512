0bf9045cf9fa008853e13f8f152e3a5dcbbb2b07
Jay Logelin <lodge@protocol.ai>
2023-03-29 11:50:28 -0300

    updates

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	go build -o testcommp
+
+run:
+	go run main.go
+
+clean:
+	rm ./testcommp
--- main.go
+++ main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
-	sha256 "github.com/minio/sha256-simd"
+	sha256simd "github.com/minio/sha256-simd"
+
+	"crypto/sha256"
 )
 
 func main() {
-	server := sha256.NewAvx512Server()
-	h512 := sha256.NewAvx512(server)
-	h512.Write([]byte("hello world"))
-	digest := h512.Sum([]byte{})
-	fmt.Println(digest)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide an argument")
+		return
+	}
+	iterations, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Failed to convert string to int:", err)
+		return
+	}
+
+	start := time.Now()
+	for i := 0; i < iterations; i++ {
+		sha256.Sum256([]byte("hello world"))
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Normal Elapsed time: %s\n", elapsed)
+
+	server := sha256simd.NewAvx512Server()
+	h512 := sha256simd.NewAvx512(server)
+
+	start := time.Now()
+	for i := 0; i < iterations; i++ {
+		h512.Write([]byte("hello world"))
+		h512.Sum([]byte{})
+	}
+	elapsed = time.Since(start)
+	fmt.Printf("AVX512 Elapsed time: %s\n", elapsed)
+
 }
